22c41e7f3d1e6ec6b7cd3e29527523cd81501e8e
guy silberman <guysilberman1@gmail.com>
2020-08-29 15:07:16 +0300

    addded flip and browser css support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-document.querySelector('#enter-button').addEventListener('click', () => {
-    
-});
+const mainContentWrapper = document.querySelector('#main-content');
+const enterButton =  document.querySelector('#enter-button');
+enterButton.addEventListener('click', showForm)
+enterButton.addEventListener('touchstart', showForm);
+
+function showForm() {
+    mainContentWrapper.classList.add('show-back');
+}
