eaf4c54cfbb27af7e8d4371c5fb4feec43716db7
guy silberman <guysilberman1@gmail.com>
2020-09-07 20:46:22 +0300

    flip card

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import SignaturePad from 'signature_pad';
+
 const mainContentWrapper = document.querySelector('#main-content');
 const enterButton =  document.querySelector('#enter-button');
 enterButton.addEventListener('click', showForm)
@@ -6,3 +8,13 @@ enterButton.addEventListener('touchstart', showForm);
 function showForm() {
     mainContentWrapper.classList.add('show-back');
 }
+
+const signatureCanvas = document.querySelector('#health-signature');
+const signaturePad = new SignaturePad(signatureCanvas);
+signatureCanvas.addEventListener('selectionstart', function (selectEvent) {
+    selectEvent.stopPropagation();
+    selectEvent.preventDefault();
+});
+ 
+
+window.signaturePad = signaturePad;
